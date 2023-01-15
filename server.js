46aef8065e9241533a638630f5c39439c1556454
Johanstraus <minhcao1116@gmail.com>
2023-01-15 14:41:25 -0800

    create server file

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const indexRoutes = require("./routes/index");
+const notesRoutes = require("./routes/notes");
+
+// Creating port and starting server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
+//Use routes information
+app.use('/api', notesRoutes);
+app.use('/', indexRoutes)
+
+//Listening port
+app.listen(PORT, () =>
+    console.log(`Server available at http://localhost:${PORT}`)
+);
